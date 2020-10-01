f55590c7b8811092c29ca67ddd83d96b6ec193b2
Firdavs <FirdavsUrunov>
2020-10-01 15:20:39 +0500

    v1 finish

--- pkg/wallet/serevices.go
+++ pkg/wallet/serevices.go
@@ -1,4 +1,4 @@
-package waller
+package wallet
 
 import (
 	"errors"
@@ -6,9 +6,11 @@ import (
 	"github.com/Firdavs2002/wallet/pkg/types"
 )
 
-var ErrPhoneRigistered = errors.New("phone already registered")
-var ErrAmountMustBePositive = errors.New("amount must be greated than zero")
-var ErrAccountNotFound = errors.New("account not found")
+var (
+	ErrPhoneRegistered      = errors.New("phone already registered")         // ErrPhoneRegistered телефон уже регитрирован
+	ErrAmountMustBePositive = errors.New("amount must be greated than zero") // счёт не может быть пустым
+	ErrAccountNotFound      = errors.New("account not found")                // пользователь не найден
+)
 
 // Services представляет информацию о пользователе.
 type Services struct {
@@ -21,7 +23,7 @@ type Services struct {
 func (s *Services) RegisterAccount(phone types.Phone) (*types.Account, error) {
 	for _, account := range s.accounts {
 		if account.Phone == phone {
-			return nil, ErrPhoneRigistered
+			return nil, ErrPhoneRegistered
 		}
 	}
 	s.nextAccountID++
@@ -47,21 +49,26 @@ func (s *Services) Deposit(accountID int64, amount types.Money) error {
 		}
 	}
 	if account == nil {
-		return ErrPhoneRigistered
+		return ErrPhoneRegistered
 	}
 
 	account.Balance += amount
 	return nil
 }
 
-// FindAccoundByIdmethod ищем пользователя по ID
-func (s *Services) FindAccoundByIdmethod(accoundID int64) (*types.Account, error) {
-	var accound *types.Account
+// FindAccountByIdmethod ищем пользователя по ID
+func (s *Services) FindAccountByIdmethod(accountID int64) (*types.Account, error) {
+	var account *types.Account
 	for _, acc := range s.accounts {
-		if acc.ID == accoundID {
-			accound = acc
+		if acc.ID == accountID {
+			account = acc
 			break
 		}
 	}
-	return accound, nil
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	return account, nil
 }
